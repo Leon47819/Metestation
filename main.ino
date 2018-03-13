7a90ccc0a8dc98acdd248df0bac98012c8ee6d3e
LeoGrand <30350011+Leon47819@users.noreply.github.com>
2018-03-13 14:23:18 +0200

    Update main.ino
    Убрал высоту, т.к. она не совсем корректно отображается, соответсвенно точной информации не несет.

--- main.ino
+++ main.ino
@@ -3,14 +3,12 @@
 **/
 
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h> // Библиотека для работы с барометром https://github.com/adafruit/Adafruit-BMP280-Library
 #include <OLED_I2C.h>
 
 #define DISPLAY_TIMEOUT 1000
 #define DEBUG_BMP
-const float ALTITUDEhPA = 1026.0;
 
 Adafruit_BMP280 bmp; // I2C
 
@@ -24,7 +22,6 @@ extern uint8_t SmallFont[]; // Базовый шрифт без поддержк
 #define SHOW_VERSION      0
 #define SHOW_TEMP         1
 #define SHOW_PREASSURE    2
-#define SHOW_ALTITUDE     3
 int state = SHOW_VERSION;
 unsigned long nextSwitch = 0;
 
@@ -34,14 +31,13 @@ void setup(){
   myOLED.begin();
   bmp.begin(0x76);
   myOLED.setFont(RusFont);
-  nextSwitch = millis()+1000;
+  nextSwitch = millis()+3000;
 }
 
 void readBMP(){
   float currentTemp = bmp.readTemperature();
   float currentPressure = bmp.readPressure();
-  float currentAltitude = bmp.readAltitude(ALTITUDEhPA);
-
+  
   #ifdef DEBUG_BMP
     Serial.print("Temperature = ");
     Serial.print(currentTemp);
@@ -49,9 +45,6 @@ void readBMP(){
     Serial.print("Pressure = ");
     Serial.print(currentPressure);
     Serial.println(" Pa");
-    Serial.print("Approx altitude = ");
-    Serial.print(currentAltitude); // this should be adjusted to your local forcase
-    Serial.println(" m");
     Serial.println();
   #endif
 }
@@ -79,30 +72,18 @@ void showPreassure(){
   myOLED.print(String(P , 1), CENTER, 15); // Отображение значения давления, с точностью до десятых
   myOLED.update();
   if (millis()>nextSwitch){
-    state = SHOW_ALTITUDE;
-    nextSwitch = millis()+5000;
-  }
-}
-
-void showAltitude(){
-  myOLED.clrScr(); // очищаем экран
-  myOLED.setFont(RusFont);
-  myOLED.print("Dscjnf v", CENTER, 3); // вывод текста русскими буквами
-  myOLED.setFont(MegaNumbers);
-  myOLED.print(String(bmp.readAltitude(ALTITUDEhPA) , 1), CENTER, 15); // Отображение значения высоты
-  myOLED.update();  
-  if (millis()>nextSwitch){
     state = SHOW_TEMP;
     nextSwitch = millis()+5000;
   }
+
 }
 
 void showVersion(){
   myOLED.clrScr(); // очищаем экран
   myOLED.setFont(RusFont);
-  myOLED.print("Dthcbz", CENTER, 3); // вывод текста русскими буквами
+  myOLED.print("DTHCBZ 12/03/2018", CENTER, 3); // вывод текста русскими буквами
   myOLED.setFont(RusFont);
-  myOLED.print("20180128", CENTER, 25); // Отображение значения высоты
+  myOLED.print("PFUHEPRF LFYYS{", CENTER, 25); // Отображение значения высоты
   myOLED.update();  
   if (millis()>nextSwitch){
     state = SHOW_TEMP;
@@ -117,9 +98,6 @@ void loop(){
       break;
     case SHOW_PREASSURE:
       showPreassure();
-      break;
-    case SHOW_ALTITUDE:
-      showAltitude();
       break;
     case SHOW_VERSION:
       showVersion();
